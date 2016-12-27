53564482f8d47192ce3d79913fa38db299ce1e0a
Rewat Khanal <rkhan016@fiu.edu>
2016-12-27 15:49:58 -0500

    Update shiny_server.R

--- shiny_server.R
+++ shiny_server.R
@@ -1,11 +1,3 @@
-#rsconnect::setAccountInfo(name='padukadan',
-                          #token='51D7FD1D3105A21D5DA11D0FF9C8E7FD',
-                          #secret='<SECRET>')
-
-
-#shinyapps::setAccountInfo(name='vasuji',
-                          #token='FC4E1EA6B8457D70BD78B8CC4BC692C2',
-                          #secret='74SC6XolP1gArG9k+MeMy0KyyphKx0fxa+9hCRN9')
 
 
 #================ local===========
